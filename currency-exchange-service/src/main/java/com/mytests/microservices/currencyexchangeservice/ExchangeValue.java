dde0a258a5a16be3b8206a54cddf2ad607500f80
Katia <katiacirlene@bitbucket.org>
2018-04-30 15:57:28 -0300

    configure JPA and initialize data.

--- currency-exchange-service/src/main/java/com/mytests/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/mytests/microservices/currencyexchangeservice/ExchangeValue.java
@@ -2,12 +2,20 @@ package com.mytests.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class ExchangeValue {
 
+	@Id
 	private Long id;
 	
+	@Column(name="currency_from")
 	private String from;
 	
+	@Column(name="currency_to")
 	private String to;
 	
 	private BigDecimal conversionMultiple;
