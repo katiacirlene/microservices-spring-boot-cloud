25961a390152376684c51142818c1d6ecd2fd817
Katia <katiacirlene@bitbucket.org>
2018-04-30 15:34:49 -0300

    setting up a "dynamic port" on the response.

--- currency-exchange-service/src/main/java/com/mytests/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/mytests/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,16 +2,26 @@ package com.mytests.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment environment;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		
+		ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		exchangeValue.setPort(
+				Integer.parseInt(environment.getProperty("local.server.port"))); 
+		
+		return exchangeValue;
 	}
 	
 }
--- currency-exchange-service/src/main/java/com/mytests/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/mytests/microservices/currencyexchangeservice/ExchangeValue.java
@@ -11,6 +11,8 @@ public class ExchangeValue {
 	private String to;
 	
 	private BigDecimal conversionMultiple;
+	
+	private int port;
 
 	public ExchangeValue() {
 
@@ -37,6 +39,14 @@ public class ExchangeValue {
 
 	public BigDecimal getConversionMultiple() {
 		return conversionMultiple;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
 	}
 	
 }
