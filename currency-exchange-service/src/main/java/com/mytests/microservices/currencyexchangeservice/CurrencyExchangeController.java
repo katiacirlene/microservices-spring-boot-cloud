41d293e73b46d26643b686b1db8bd7ca9d2db4de
Katia <katiacirlene@bitbucket.org>
2018-05-01 13:44:37 -0300

    removing unused imports.

--- currency-exchange-service/src/main/java/com/mytests/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/mytests/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.mytests.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
