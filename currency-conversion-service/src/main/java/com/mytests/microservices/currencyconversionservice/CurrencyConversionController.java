cc86b5e590523675e9546cb2e25d411367526f9c
Katia <katiacirlene@bitbucket.org>
2018-05-01 17:18:35 -0300

    using Feign REST Client for Service Invocation.

--- currency-conversion-service/src/main/java/com/mytests/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/mytests/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
 
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
@@ -33,5 +37,17 @@ public class CurrencyConversionController {
 		return new CurrencyConversionBean(response.getId(), from, to, conversionMultiple, quantity,
 				quantity.multiply(conversionMultiple), response.getPort());		
 	}
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+		
+		BigDecimal conversionMultiple = response.getConversionMultiple();
+		
+		return new CurrencyConversionBean(response.getId(), from, to, conversionMultiple, quantity,
+				quantity.multiply(conversionMultiple), response.getPort());		
+	}	
 	
 }
