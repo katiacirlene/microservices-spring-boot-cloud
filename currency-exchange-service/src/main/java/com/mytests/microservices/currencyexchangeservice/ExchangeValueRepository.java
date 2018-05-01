606384331a1d063146d0abde5c8c54f779f89e88
Katia <katiacirlene@bitbucket.org>
2018-05-01 13:42:01 -0300

    creating a JPA repository.

---
+++ currency-exchange-service/src/main/java/com/mytests/microservices/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.mytests.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+
+	ExchangeValue findByFromAndTo(String from, String to);
+	
+}
