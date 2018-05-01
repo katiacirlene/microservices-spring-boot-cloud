2cd420bd4b56dc16227648d1ed4c1694babb95b8
Katia <katiacirlene@bitbucket.org>
2018-05-01 14:21:19 -0300

    creating a hard coded "Currency Conversion Service".

---
+++ currency-conversion-service/src/main/java/com/mytests/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,19 @@
+package com.mytests.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyConversionController {
+
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		return new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
+	}
+	
+}
